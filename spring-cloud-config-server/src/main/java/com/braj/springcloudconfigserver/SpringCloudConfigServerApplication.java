b43d01a37c5ce014fe091c689318b187306e6e95
Basavaraj <braj.angadi@gmail.com>
2019-04-18 23:36:10 +0530

    Spring Cloud Configuration

---
+++ spring-cloud-config-server/src/main/java/com/braj/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,23 @@
+package com.braj.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RestController;
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
+
+@RestController
+@RefreshScope
+class refresh{
+	
+	
+}
